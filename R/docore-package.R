b30cfcef6c07234f7f64ece5c394802b19068f7d
obreschkow <danail.obreschkow@gmail.com>
2022-02-03 12:05:55 +0800

    v1.1

---
+++ R/docore-package.R
@@ -0,0 +1,9 @@
+#' @title Utility Functions for Scientific Data Analysis
+#'
+#' @description Basic routines used in scientific coding, such as timing routines, vector/array handing functions and I/O support routines.
+#'
+#' @author Danail Obreschkow
+#'
+#' @docType package
+#' @name docore
+NULL
